e4e8f5473a92a481847cae51cb19155088035cb7
rmdocherty <rmdocherty2@gmail.com>
2021-08-07 22:05:01 +0100

    Added measurement.py and Measurements(), a class to perform a given measurement on a given QC. Currently can calc the QFI for a QC and the resulting effective quantum dimension.

---
+++ measurement.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  7 21:20:44 2021
+
+@author: ronan
+"""
+import qutip as qp
+import numpy as np
+import scipy
+
+
+class Measurements():
+    def __init__(self, QC):
+        self._QC = QC
+
+    def _get_QFI(self):
+        """
+        Given the input QC and it's gradient state list, calculate the assoicated
+        QFI matrix by finding F_i,j = Re{<d_i psi| d_j psi>} - <d_i psi|psi><psi|d_j psi>
+        for each i,j in n_params.
+
+        Returns:
+            qfi_matrix : np.array
+            A n_param * n_param matrix of the QFI matrix for the VQC.
+        """
+        n_params = self._QC._n_params #these should both probably be getter methods but still
+        grad_state_list = self._QC._gradient_state_list
+
+        #get all single elements first
+        single_qfi_elements = np.zeros(n_params, dtype=np.complex128)
+        for param in range(n_params):
+            overlap = self._QC._ground_state.overlap(grad_state_list[param])
+            single_qfi_elements[param] = overlap
+
+        qfi_matrix = np.zeros([n_params, n_params])
+        for p in range(n_params):
+            for q in range(p, n_params):
+                deriv_overlap = grad_state_list[p].overlap(grad_state_list[q])
+                #single_qfi_elements[i] is <d_i psi | psi>
+                RHS = np.conjugate(single_qfi_elements[p]) * single_qfi_elements[q]
+                #assign p, qth elem of QFI, c.f eq (B3) in NIST review
+                qfi_matrix[p, q] = np.real(deriv_overlap - RHS)
+
+        for p in range(n_params): #use fact QFI mat. real, hermitian and therefore symmetric
+            for q in range(p + 1, n_params):
+                qfi_matrix[q, p] = qfi_matrix[p, q]
+        return qfi_matrix
+
+    def get_effective_quantum_dimension(self, cutoff_eigvals):
+        QFI = self._get_QFI()
+        eigvals, eigvecs = scipy.linalg.eigh(QFI)
+        nonzero_eigvals = eigvals[eigvals > cutoff_eigvals]
+        eff_quant_dim = len(nonzero_eigvals)
+        return eff_quant_dim
+        
--- tests.py
+++ tests.py
@@ -8,6 +8,7 @@ Created on Sat Aug  7 18:12:45 2021
 Does our QC code match up to the results from Tobias' code?
 """
 from QC import QuantumCircuit
+from measurement import Measurements
 
 q = QuantumCircuit(4, 3, "chain", "cnot")
 print("4 qubit chain topology connected indices")
@@ -19,3 +20,8 @@ print(q2._gen_entanglement_indices())
 
 #%%
 q.run() #should ouput 0.46135870050914374
+
+#%%
+M = Measurements(q)
+EFD = M.get_effective_quantum_dimension(10**-12)
+print(f"Effective quantum dimension is {EFD}")
