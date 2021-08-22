Fixed experessibility measurement by normalising the P_haar - this is valid because both P(x) and Q(x) must be probability distributions (i.e normalised) Started adding some more documentation to functions in measurement.py .

measurement.py
@@ -8,6 +8,7 @@ Created on Sat Aug  7 21:20:44 2021
 import qutip as qp
 import numpy as np
 import scipy
+import matplotlib.pyplot as plt
 
 
 class Measurements():
@@ -59,26 +60,54 @@ class Measurements():
         eff_quant_dim = len(nonzero_eigvals)
         return eff_quant_dim
 
-    def expressibility(self, sample_N):
+    def _gen_f_samples(self, sample_N):
+        """
+        Generate random psi_theta and psi_pi $sample_N times for given PQC, then calculate
+        |<psi_theta | psi_phi>|^2 which is F (1st moment of frame potential).
+        Returns:
+            F_samples = List of floats
+        """
         F_samples = []
-        N = 2 ** self._QC._n_qubits
         for i in range(sample_N):
-            #print(i)
-            self._QC.gen_quantum_state()
+            self._QC.gen_quantum_state(energy_out=False)
             state1 = self._QC._quantum_state #psi theta
-            self._QC.gen_quantum_state()
+            self._QC.gen_quantum_state(energy_out=False)
             state2 = self._QC._quantum_state #psi phi
             sqrt_F = state1.overlap(state2)
-            F = np.conjugate(sqrt_F) * sqrt_F
-            #print(type(F))
+            F = sqrt_F * sqrt_F
             F_samples.append(np.real(F))
-        prob, F = np.histogram(F_samples, bins="fd")
-        print(prob)
+        return F_samples
+
+    def _gen_histo(self, F_samples):
+        """
+        Generate the probability mass histogram (i.e sum(P_pqc) = 1) for the F_samples
+        from the PQC.
+        Returns:
+            prob: List of floats, 0 < p < 1 that are probabilities of state pair with Fidelity F
+            F: List of floats, 0 < f < 1 that are fidelity midpoints,
+        """
+        #bin no. = 75 from paper
+        prob, edges = np.histogram(F_samples, bins=75, range=(0, 1))
+        prob = prob / sum(prob) #normalise by sum of prob or length?
+        #this F assumes bins go from 0 to 1. Calculate midpoints of bins from np.hist
+        F = np.array([(edges[0] + edges[1]) / 2] + [(edges[i - 1] + edges[i]) / 2 for i in range(2, len(edges))])
+        return prob, F
+
+    def expressibility(self, sample_N, graphs=False):
+        N = 2 ** self._QC._n_qubits
+        F_samples = self._gen_f_samples(sample_N)
+        prob, F = self._gen_histo(F_samples, graphs=True)
         expr = 0
-        for index, P_pqc in enumerate(prob):
-            #print(index)
-            P_haar = (N - 1) * (1 - F[index]) ** (N - 2)
-            expr += P_pqc * np.log(P_pqc / P_haar)
+        haar = (N - 1) * ((1 - F) ** (N - 2))
+        P_haar = haar / sum(haar) #do i need to normalise this?
+        for index, P_pqc in enumerate(prob): #f = 1 at last bin so this is div by 0. fix by doing bin middles instead
+            log = np.log(P_pqc / P_haar[index]) if P_pqc > 0 else 0
+            expr += P_pqc * log
+        if graphs is True:
+            plt.plot(F, P_haar, label="Haar", color="C0", alpha=0.7)
+            plt.plot(F, prob, label="Quantum state", color="C1", alpha=0.7)
+            plt.xlabel("Fidelity")
+            plt.ylabel("Probability")
+            plt.legend(fontsize=20)
         print(f"Expressibility is {expr}")
         return expr
-