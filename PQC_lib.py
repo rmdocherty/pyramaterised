CHanged previous sqrtH definition to be a fixed Ry rotation of pi/4, and made an actual sqrtH. Added some tests for EoM to do with haar states but not cirrently working

PQC_lib.py
@@ -122,33 +122,42 @@ class H(PRot):
 
 
 class sqrtH(H):
-    """Sqrt hadarmard, used in the quantum geometry circuit."""
-
     def _set_op(self):
-        """sqrt Hadamard gate is just R_y(pi/4)"""
-        self._theta = np.pi / 4
         ops = qt.qip.operations
         self._gate = ops.ry
-        return self._gate(np.pi / 4, N=self._q_N, target=self._q_on)
+        return np.sqrt(ops.x_gate(self._q_N, self._q_on) * self._gate(np.pi / 2, N=self._q_N, target=self._q_on))
 
 
-class fixed_R_y(sqrtH):
-    """Fixed R_y rotation by pi/2. Needs to inherit from sqrtH so that it
-    isn't counted as a parameterised gate later in NPQC code."""
+class fixed_R_y(R_y):
+    """Fixed R_y rotation by angle theta. Isn't parameterised and angle can't
+    be changed after initialization."""
 
-    def _set_op(self):
-        self._theta = np.pi / 2
-        ops = qt.qip.operations
-        self._gate = ops.ry
-        return self._gate(np.pi / 2, N=self._q_N, target=self._q_on)
+    def __init__(self, q_on, q_N, theta):
+        self._q_on = q_on
+        self._q_N = q_N
+        self._theta = theta
+        self._is_param = False
+        self._operation = self._set_op()
+
+    def set_theta(self, theta):
+        return None
 
 
-class S(sqrtH):
+class S(H):
     def _set_op(self):
         self._theta = np.pi / 2
         ops = qt.qip.operations
         self._gate = ops.phasegate
         return self._gate(np.pi / 2, N=self._q_N, target=self._q_on)
+
+
+class T(H):
+    """T-gate."""
+
+    def _set_op(self):
+        ops = qt.qip.operations
+        self._gate = ops.t_gate
+        return self._gate(N=self._q_N, target=self._q_on)
 
 #%% Entangling gates
 
@@ -247,6 +256,8 @@ class ALLTOALL(EntGate):
 
 #%% 2 qubit rotation gates
 
+#big question - should the second qubit angle be -1 * theta ???
+
 
 class R_zz(PRot, EntGate):
     def __init__(self, qs_on, q_N):
@@ -260,7 +271,7 @@ class R_zz(PRot, EntGate):
         self._gate = qt.qip.operations.rz
         self._pauli = qt.sigmaz() #are these derivatives right?
         g1 = self._gate(self._theta, N=self._q_N, target=self._q1)
-        g2 = self._gate(self._theta, N=self._q_N, target=self._q2)
+        g2 = self._gate(-1 * self._theta, N=self._q_N, target=self._q2)
         return qt.tensor(g1, g2)
 
     def __repr__(self):
@@ -274,7 +285,7 @@ class R_xx(R_zz):
         self._gate = qt.qip.operations.rx
         self._pauli = qt.sigmax()
         g1 = self._gate(self._theta, N=self._q_N, target=self._q1)
-        g2 = self._gate(self._theta, N=self._q_N, target=self._q2)
+        g2 = self._gate(-1 * self._theta, N=self._q_N, target=self._q2)
         return qt.tensor(g1, g2)
 
 
@@ -283,7 +294,7 @@ class R_yy(R_zz):
         self._gate = qt.qip.operations.ry
         self._pauli = qt.sigmay()
         g1 = self._gate(self._theta, N=self._q_N, target=self._q1)
-        g2 = self._gate(self._theta, N=self._q_N, target=self._q2)
+        g2 = self._gate(-1 * self._theta, N=self._q_N, target=self._q2)
         return qt.tensor(g1, g2)
 
 

new_tests.py
@@ -134,7 +134,7 @@ print(f"Circuit 11 expr for {L} layers is  {c11_out['Expr']}")
 #%% =============================QUANTUM GEOMETRY CIRCUIT TESTS=============================
 """Tests based on default circuit in arXiv:2102.01659v1 github"""
 qg_circuit = pqc.PQC(4)
-init_layer = [pqc.sqrtH(i, 4) for i in range(4)]
+init_layer = [pqc.fixed_R_y(i, 4, np.pi / 4) for i in range(4)]
 layer1 = [pqc.R_z(0, 4), pqc.R_x(1, 4), pqc.R_y(2, 4), pqc.R_z(3, 4), pqc.CHAIN(pqc.CNOT, 4)]
 layer2 = [pqc.R_x(0, 4), pqc.R_x(1, 4), pqc.R_x(2, 4), pqc.R_y(3, 4), pqc.CHAIN(pqc.CNOT, 4)]
 layer3 = [pqc.R_z(0, 4), pqc.R_x(1, 4), pqc.R_y(2, 4), pqc.R_y(3, 4), pqc.CHAIN(pqc.CNOT, 4)]
@@ -189,13 +189,13 @@ def gen_clifford_circuit(p, N):
         layer = []
         for n in range(N):
             gate = random.choice(clifford_gates)
-            if type(gate) == pqc.PRot:
+            if type(gate) == pqc.PRot: #can't check is_param of this as not instantised yet - could make class variable?
                 q_on = random.randint(0, N - 1)
                 layer.append(gate(q_on, N))
-            elif type(gate) == pqc.EntGate:
+            elif type(gate) == pqc.EntGate: #entangling gate
                 qs = range(N)
-                q_1 = random.sample(qs) #use sample so can't pick same option twice
-                q_2 = random.sample(qs)
+                q_1, q_2 = random.sample(qs, k=2) #use sample so can't pick same option twice
+                print(q_1, q_2)
                 layer.append(gate([q_1, q_2], N))
             layers.append(layer)
     return layers
@@ -212,7 +212,7 @@ max_N = 6
 max_P = 12
 
 entropies = []
-for n in range(1, max_N):
+for n in range(2, max_N):
     for p in range(1, max_P):
         layers = gen_clifford_circuit(p, n)
         clifford_circuit = pqc.PQC(n)
@@ -224,6 +224,23 @@ for n in range(1, max_N):
         entropies.append(e)
 
 print(f"Entropies of magic are {entropies}, should be roughly 0") #values are ~0 for all so further proff code is working.
+
+#%% Test by inserting T gates into Clifford circuits
+
+
+#%% Test by looking at magic of Haar states (should be high)
+class Haar():
+    def __init__(self, N):
+        self._n_qubits = N
+        self._quantum_state = qt.random_objects.rand_ket_haar(2**N)
+
+    def gen_quantum_state(self):
+        return qt.random_objects.rand_ket_haar(2**N)
+
+haar = Haar(2)
+haar_m = Measurements(haar)
+magic = haar_m.efficient_measurements(100, expr=False, ent=False, eom=True)
+
 
 #%% =============================NPQC TESTS=============================
 """